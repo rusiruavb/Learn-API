d36c4a5f5e69529041d6ea192355c44a23e7d67c
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-01-30 23:57:25 +0530

    Modify the response handling middleware

--- src/lib/response.handler.js
+++ src/lib/response.handler.js
@@ -3,7 +3,7 @@ export default {
     return function(data) {
       console.log(data);
       if (data == null) {
-        return res.status(404).json({status: 404, message: "Not found!"})
+        return res.status(404).json({status: 404, message: "Not found!"});
       }
       return res.status(status).json(data);
     }
@@ -13,24 +13,19 @@ export default {
     return function(data) {
       console.log(data)
       if (data == null) {
-        return res.status(status).json({status: 404, message: "Not found!"})
+        return res.status(status).json({status: 404, message: "Not found!"});
       }
       return data
     }
   },
 
-  handleError: function(res, status = 500) {
-    return function(err, message) {
-      console.log("--------------Error: ",err.message);
-      if (err.name) {
-        switch(err.name) {
-          case "SequelizeUniqueConstraintError":
-            return res.status(status).json({status: 500, message: message});
-          default: 
-            return res.status(400).json({ status: 'ERROR', details: err.message });
-        }
+  handleError: function(res, status = 500, message) {
+    return function(data) {
+      console.log("----Error: ", data);
+      if (data == null) {
+        return res.status(404).json({status: 404, message: "Not found!"});
       }
-      return res.status(status).json({status: status, message: message });
+      return res.status(status).json({status: status, message: "Error"});
     }
   }
 }
